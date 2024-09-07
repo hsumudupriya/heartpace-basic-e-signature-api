300ccccc134b121198415b4189f3e3fc8e26980f
Hasanta Sumudupriya <hsumudupriya@gmail.com>
2024-09-07 20:16:35 +0530

    Code refactoring.
    Refactored the routes\api.php file and removed code redundancies.

--- routes/api.php
+++ routes/api.php
@@ -4,23 +4,21 @@ use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SignatureRequestController;
+use App\Http\Controllers\SignDocumentController;
 use Illuminate\Support\Facades\Route;
 
 // Authentication routes
 Route::post('login', LoginController::class)->name('login');
 Route::post('register', RegisterController::class)->name('register');
 
-// Document routes
+// Routes protected by access token
 Route::middleware([
     'auth:sanctum',
 ])->group(function () {
+    // Document routes
     Route::apiResource('documents', DocumentController::class)->only(['index', 'store']);
-});
 
-// Signasture Request routes
-Route::middleware([
-    'auth:sanctum',
-])->group(function () {
+    // Signasture Request routes
     Route::get('/signature-requests/received-requests', [SignatureRequestController::class, 'receivedSignatureRequests'])
         ->name('signature-requests.received-requests');
     Route::apiResource('signature-requests', SignatureRequestController::class)->only(['index', 'store']);
